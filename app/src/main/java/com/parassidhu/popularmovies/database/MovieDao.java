21702cce536b33c96d5820598baab579d8a1527b
Paras Sidhu <sidhuparas1234@gmail.com>
2018-07-12 15:15:22 +0530

    Added Dao

---
+++ app/src/main/java/com/parassidhu/popularmovies/database/MovieDao.java
@@ -0,0 +1,31 @@
+package com.parassidhu.popularmovies.database;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import com.parassidhu.popularmovies.models.MovieItem;
+
+import java.util.List;
+
+@Dao
+public interface MovieDao {
+
+    @Insert
+    void insertMovie(MovieItem movie);
+
+    @Delete
+    void deleteMovie(MovieItem movie);
+
+    @Update
+    void updateMovie(MovieItem movie);
+
+    @Query("SELECT * FROM movies")
+    LiveData<List<MovieItem>> getMovies();
+
+    @Query("SELECT * FROM movies WHERE id=:id")
+    MovieItem getMovieById(int id);
+}
